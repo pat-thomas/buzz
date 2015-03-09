da1b31a680770d78c25dfd2e544932c4ace31847
Pat Thomas <pthomas@relaynetwork.com>
2015-03-09 12:11:27 -0400

    Initial commit, core.typed and core.async dependencies.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to buzz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/buzz/core_test.clj
@@ -0,0 +1,7 @@
+(ns buzz.core-test
+  (:require [clojure.test :refer :all]
+            [buzz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
