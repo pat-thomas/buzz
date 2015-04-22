23062228d330aa14f326fe7d6ab40c13a2d58b51
Pat Thomas <pthomas@relaynetwork.com>
2015-04-21 23:17:38 -0400

    Move steel-drum into sounds namespace.

--- src/buzz/core.clj
+++ src/buzz/core.clj
@@ -1,5 +1,6 @@
 (ns buzz.core
   (:require [buzz.loop          :as loop]
+            [buzz.sounds        :as sounds]
             [clojure.core.async :as async :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]
             [overtone.midi      :as midi]
             [overtone.live      :as o]))
@@ -7,19 +8,6 @@
 (def main-chan (chan))
 (def midi-chan (chan 1))
 (def timeout-value 400)
-
-(defn find-kbd-controller
-  []
-  (first (filter #(= (:name %) "USB Oxygen 8 v2")
-                 (midi/midi-devices))))
-
-(o/definst steel-drum
-  [note 60 amp 0.8]
-  (let [freq (o/midicps note)]
-    (* amp
-       (o/env-gen (o/perc 0.01 0.05) 1 1 0 1 :action o/FREE)
-       (+ (o/sin-osc (/ freq 2))
-          (o/rlpf (o/saw freq) (* 1.1 freq) 0.4)))))
 
 (defn listen-for-midi-events
   []
@@ -35,7 +23,7 @@
   [{:keys [note velocity] :as e}]
   (go
     (dotimes [_ 5]
-      (steel-drum note velocity)
+      (sounds/steel-drum note velocity)
       (<! (timeout (+ 65 (rand-int 45)))))))
 
 (defn main
--- src/buzz/sounds.clj
+++ src/buzz/sounds.clj
@@ -1,5 +1,15 @@
 (ns buzz.sounds
-  (:require [overtone.live :as overtone]))
+  (:require [overtone.live :as o]))
 
-(def kick (overtone/freesound 148634))
-(def hat (overtone/freesound 53525))
+(def kick (o/freesound 148634))
+(def hat (o/freesound 53525))
+
+(o/definst steel-drum
+  [note 60 amp 0.8]
+  (let [freq (o/midicps note)]
+    (* amp
+       (o/env-gen (o/perc 0.01 0.05) 1 1 0 1 :action o/FREE)
+       (+ (o/sin-osc (/ freq 2))
+          (o/rlpf (o/saw freq) (* 1.1 freq) 0.4)))))
+
+
