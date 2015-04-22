64e598e1d0a43ba963ee8568689d54dfce03d668
Pat Thomas <pthomas@relaynetwork.com>
2015-04-21 22:44:48 -0400

    Bleeps and bloops.

--- src/buzz/core.clj
+++ src/buzz/core.clj
@@ -4,23 +4,39 @@
             [overtone.midi      :as midi]
             [overtone.live      :as o]))
 
-(do
-  (def stop (atom false))
-  (def main-chan (chan))
-  (def midi-chan (chan 1))
-  (def timeout-value 2000))
+(def main-chan (chan))
+(def midi-chan (chan 1))
+(def timeout-value 400)
+
+(defn find-kbd-controller
+  []
+  (first (filter #(= (:name %) "USB Oxygen 8 v2")
+                 (midi/midi-devices))))
+
+(o/definst steel-drum
+  [note 60 amp 0.8]
+  (let [freq (o/midicps note)]
+    (* amp
+       (o/env-gen (o/perc 0.01 0.05) 1 1 0 1 :action o/FREE)
+       (+ (o/sin-osc (/ freq 2))
+          (o/rlpf (o/saw freq) (* 1.1 freq) 0.4)))))
 
 (defn listen-for-midi-events
   []
   (o/on-event
    [:midi :note-on]
    (fn [e]
-     (>! midi-chan e))
+     (println "publishing event...")
+     (go
+       (>! midi-chan e)))
    ::keyboard-handler))
 
 (defn handler
-  [e]
-  (println "got event:" e))
+  [{:keys [note velocity] :as e}]
+  (go
+    (dotimes [_ 5]
+      (steel-drum note velocity)
+      (<! (timeout (+ 65 (rand-int 45)))))))
 
 (defn main
   []
@@ -31,8 +47,12 @@
   (loop/timed-async-loop timeout-value main-chan handler))
 
 (comment
-  (main)
-  (go
-    (dotimes [_ 10]
-      (>! main-chan (rand-nth [:foof :...meow]))))
+  (do
+    (loop/stop)
+    (loop/start))
+  (do
+    (main)
+    (go
+      (dotimes [_ 10]
+        (>! main-chan (rand-nth [:foof :...meow])))))
   )
--- src/buzz/loop.clj
+++ src/buzz/loop.clj
@@ -1,15 +1,25 @@
 (ns buzz.loop
   (:require [clojure.core.async :as async :refer [alts! go chan <! >! <!! >!! timeout]]))
 
+(def stop-val (atom false))
+
+(defn stop
+  []
+  (reset! stop-val true))
+
+(defn start
+  []
+  (reset! stop-val false))
+
 (defn timed-async-loop
   [wait input-ch handler]
   (let [ch (chan)]
     (go
-      (while true
+      (while (not @stop-val)
         (<! (timeout wait))
         (handler (<! ch))))
     (go
-      (while true
+      (while (not @stop-val)
         (<! (timeout wait))
         (>! ch (<! input-ch))))
     input-ch))
